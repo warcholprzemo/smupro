bdaf876d46f2a2e3bbefb7b88e6c3f194c2fa437
Przemyslaw Warchol <przemyslaw@unity3d.com>
2018-09-22 18:54:31 +0200

    Use psql as database

--- smupro/settings.py
+++ smupro/settings.py
@@ -83,10 +83,23 @@ WSGI_APPLICATION = 'smupro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smupro',
+        'USER': 'smutnyuser',
+        'PASSWORD': 'smutnyuser',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
