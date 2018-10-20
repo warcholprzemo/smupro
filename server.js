69adc72a04dd1d36985291af67b4fbfaeb1a3a78
Przemyslaw Warchol <przemyslaw@unity3d.com>
2018-10-21 00:14:47 +0200

    Try to do a magic in server.py

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.static(__dirname + "/dist"));
 
 // send the user to index html page inspite of the url
+app.get('/dist/static/index.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist/static/index.js'));
+});
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'prod-template/index.html'));
 });
