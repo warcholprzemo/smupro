7b15dc53eef78424e6fa6446c12bba7ce04856d7
Przemyslaw Warchol <przemyslaw@unity3d.com>
2018-10-20 22:25:11 +0200

    Try to use webpack and Express

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.css$/,
+                include: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {}
+                }]
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader"
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
