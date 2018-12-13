e3561a332cabe8fb8ce130af468ef4692a44e05c
Przemyslaw Warchol <przemyslaw@unity3d.com>
2018-12-13 18:19:41 +0100

    Try to fix STATIC_ROOT

--- smupro/settings.py
+++ smupro/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "dist"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'dist')
 STATICFILES_STORAGE = 'spa.storage.SPAStaticFilesStorage'
 
 
