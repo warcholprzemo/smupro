78fc9e4ab85fa700c70ab45ebca24f55b82573f1
Przemyslaw Warchol <przemyslaw@unity3d.com>
2018-12-29 19:43:38 +0100

    Try to use serve for MEDIA_URL

--- smupro/urls.py
+++ smupro/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
+from django.views.static import serve
 
 from multiplex.views import HallList
 from pocket.views import BlogList, SomeDataList, SomeDataPost, BlogDetail, MyImageCreate, MyImageList
@@ -61,6 +62,7 @@ urlpatterns = [
 
 if settings.DEBUG is False:
     extensions = staticfiles_urlpatterns()
+    extensions += [path('media/<str:path>', serve, {'document_root': settings.MEDIA_ROOT})]
     extensions += [re_path('.*', TemplateView.as_view(template_name='index.html')),]
 else:
     extensions = [
