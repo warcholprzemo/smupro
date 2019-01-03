38b99f3b7a76fd7fe4e75c8b9b22ecbe3ef71534
Przemyslaw Warchol <przemyslaw@unity3d.com>
2019-01-04 00:42:11 +0100

    Try to setup publicPath for images

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,9 @@ module.exports = {
                 test: /\.(png|jpg|gif)$/,
                 use: [{
                     loader: 'file-loader',
-                    options: {}
+                    options: {
+                        publicPath: "./"
+                    }
                 }]
             },
             {
