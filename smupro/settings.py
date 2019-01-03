e829aed1ebd3409f33ea514df08bdee2ecf12ff9
Przemyslaw Warchol <przemyslaw@unity3d.com>
2019-01-03 22:35:00 +0100

    Disable DEFAULT_FILE_STORAGE. Move AWS setting to the end of settings

--- smupro/settings.py
+++ smupro/settings.py
@@ -124,12 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'warcholprzemo-bucket'
-
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 
 #ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
@@ -139,7 +134,7 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, "dist"),  # copy files from here into staticfiles (WhiteNoiseMiddleware likes it!)
 ]
 #STATIC_URL = '/static/'                                 # WhiteNoiseMiddleware likes it!
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')     # WhiteNoiseMiddleware likes it!
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
@@ -163,6 +158,11 @@ REST_FRAMEWORK = {
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'warcholprzemo-bucket'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
 
 try:
     from .local_settings import *
