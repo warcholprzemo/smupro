6fd79f32d1c10ae33aff7f3fef5f32e95592063f
Przemyslaw Warchol <przemyslaw@unity3d.com>
2019-01-06 17:12:04 +0100

    echh2

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
                 test: /\.(png|jpg|gif)$/,
                 loader: 'file-loader',
                 options: {
-                    outputPath: "../",
+                    name: '../[hash].[ext]'
                 }
             },
             {
