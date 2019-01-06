cdf2342ab5c4b4a9b0423ecfd23983f34afd1eef
Przemyslaw Warchol <przemyslaw@unity3d.com>
2019-01-06 17:08:38 +0100

    Try to use outputPath

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
                 test: /\.(png|jpg|gif)$/,
                 loader: 'file-loader',
                 options: {
-                    publicPath: "../",
+                    outputPath: "../",
                 }
             },
             {
