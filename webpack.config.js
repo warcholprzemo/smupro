da18bd94fb41f55e444f3e13a5170fceb9fadf33
Przemyslaw Warchol <przemyslaw@unity3d.com>
2019-01-06 17:56:28 +0100

    Back to scratch and once again with collectstatic

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
                 test: /\.(png|jpg|gif)$/,
                 loader: 'file-loader',
                 options: {
-                    name: '../[hash].[ext]'
+                    publicPath: "https://s3.eu-west-2.amazonaws.com/warcholprzemo-bucket/"
                 }
             },
             {
